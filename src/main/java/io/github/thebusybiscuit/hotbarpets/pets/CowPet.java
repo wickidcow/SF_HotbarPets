a22c1493e6faa1c3cd901ff6a85f169597245657
wickidcow <jaywago@gmail.com>
2026-09-09 20:11:22 -0400

    fix: use current potion effect names

--- src/main/java/io/github/thebusybiscuit/hotbarpets/pets/CowPet.java
+++ src/main/java/io/github/thebusybiscuit/hotbarpets/pets/CowPet.java
@@ -18,14 +18,13 @@ public class CowPet extends SimpleBasePet {
     @Override
     public void onUseItem(Player p) {
         p.removePotionEffect(PotionEffectType.BLINDNESS);
-        p.removePotionEffect(PotionEffectType.CONFUSION);
+        p.removePotionEffect(PotionEffectType.NAUSEA);
         p.removePotionEffect(PotionEffectType.HUNGER);
         p.removePotionEffect(PotionEffectType.POISON);
-        p.removePotionEffect(PotionEffectType.SLOW);
-        p.removePotionEffect(PotionEffectType.SLOW_DIGGING);
+        p.removePotionEffect(PotionEffectType.SLOWNESS);
+        p.removePotionEffect(PotionEffectType.MINING_FATIGUE);
         p.removePotionEffect(PotionEffectType.WEAKNESS);
         p.removePotionEffect(PotionEffectType.WITHER);
         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_COW_AMBIENT, 1.0F, 2.0F);
     }
-
 }
