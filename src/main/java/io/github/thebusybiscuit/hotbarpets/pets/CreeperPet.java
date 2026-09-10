62f32a9d614b2234a46af750977e3c3091d8c7cc
wickidcow <jaywago@gmail.com>
2026-09-09 20:11:13 -0400

    fix: use current TNT entity type

--- src/main/java/io/github/thebusybiscuit/hotbarpets/pets/CreeperPet.java
+++ src/main/java/io/github/thebusybiscuit/hotbarpets/pets/CreeperPet.java
@@ -9,7 +9,6 @@ import org.bukkit.metadata.FixedMetadataValue;
 
 import io.github.thebusybiscuit.hotbarpets.HotbarPets;
 import io.github.thebusybiscuit.hotbarpets.SimpleBasePet;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 
 public class CreeperPet extends SimpleBasePet {
@@ -18,17 +17,14 @@ public class CreeperPet extends SimpleBasePet {
 
     public CreeperPet(HotbarPets plugin, SlimefunItemStack item, ItemStack food, ItemStack[] recipe) {
         super(plugin.getItemGroup(), item, food, recipe);
-
         this.plugin = plugin;
     }
 
     @Override
     public void onUseItem(Player p) {
-        TNTPrimed tnt = (TNTPrimed) p.getWorld().spawnEntity(p.getLocation(), EntityType.PRIMED_TNT);
+        TNTPrimed tnt = (TNTPrimed) p.getWorld().spawnEntity(p.getLocation(), EntityType.TNT);
         tnt.setMetadata("hotbarpets_player", new FixedMetadataValue(plugin, p.getUniqueId()));
         tnt.setFuseTicks(0);
-
         p.getWorld().playSound(p.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 1.0F, 2.0F);
     }
-
 }
